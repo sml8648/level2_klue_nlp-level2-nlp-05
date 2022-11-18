cb876cdbb23935ae22ed9001b0172134f43f8a11
jihoon <1204jh@skku.edu>
2022-11-18 15:54:28 +0000

    fix baseconfig

--- train.py
+++ train.py
@@ -100,7 +100,7 @@ def train(conf):
         # `no`: No evaluation during training.
         # `steps`: Evaluate every `eval_steps`.
         # `epoch`: Evaluate every end of epoch.
-        eval_steps=conf.train.eval_steps,  # 해당 스탭마다 valid set을 이용해서 모델을 평가합니다. 이 값을 기준으로 save_steps 모델이 저장됩니다
+        eval_steps=conf.train.eval_steps,  # 해당 스탭마다 valid set을 이용해서 모델을 평가합니다. 이 값을 기준으로 save_steps 모델이 저장됩니다.
         load_best_model_at_end=True,
         # huggingface hub에 모델을 저장합니다.
         # push_to_hub=True를 설정하는 경우 trainer.save_model() 단계에서 에러가 발생합니다. 둘 중에 하나만 사용해주세요!!!
