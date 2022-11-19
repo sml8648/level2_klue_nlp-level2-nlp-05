30a007bc658987ef27dfdebeb190695ff5ecd889
jstep750 <jstep0000@gmail.com>
2022-11-19 22:27:47 +0000

    add lstm model

--- model/model.py
+++ model/model.py
@@ -96,3 +96,37 @@ class CustomModel(nn.Module):
         # carefully choose hyper-parameters
         loss = ce_loss + alpha * kl_loss
         return loss
+
+
+class LSTMModel(CustomModel):
+    def __init__(self, conf, new_vocab_size):
+        super().__init__(conf, new_vocab_size)
+        self.num_labels = 30
+        self.conf = conf
+        self.model_name = conf.model.model_name
+        self.model = AutoModel.from_pretrained(
+                        self.model_name, 
+                        hidden_dropout_prob=conf.train.dropout,
+                        attention_probs_dropout_prob=conf.train.dropout
+                    )
+        self.model.resize_token_embeddings(new_vocab_size)
+        self.hidden_dim = self.model.config.hidden_size
+        self.loss_fct = loss_module.loss_config[conf.train.loss]
+
+        self.lstm = nn.LSTM(input_size=self.hidden_dim, hidden_size=self.hidden_dim//2, num_layers=2, dropout=conf.train.dropout,
+                            batch_first=True, bidirectional=True)
+        self.activation = torch.tanh
+        self.dropout = nn.Dropout(conf.train.dropout)
+        self.out_proj = nn.Linear(self.hidden_dim, self.num_labels)
+    
+    def process(self, input_ids=None, attention_mask=None, token_type_ids=None):
+        # BERT output= (16, 244, 1024) (batch, seq_len, hidden_dim)
+        output= self.model(input_ids = input_ids, attention_mask = attention_mask, token_type_ids=token_type_ids)[0] 
+        # LSTM last hidden, cell state shape : (2, 244, 1024) (num_layer, seq_len, hidden_size)
+        lstm_output, (last_hidden, last_cell)= self.lstm(output)
+        # (16, 1024) (batch, hidden_dim)
+        cat_hidden = torch.cat((last_hidden[0], last_hidden[1]), dim = 1)
+        x = self.activation(cat_hidden)
+        x = self.dropout(x)
+        logits = self.out_proj(x)
+        return logits
