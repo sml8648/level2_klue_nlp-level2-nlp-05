ac025ef5ab5eae575425dd0fd0d52b52b476e83a
GinToFim <shlf6673@gmail.com>
2022-11-21 02:42:31 +0000

    fix tokenizer use_fast

--- inference.py
+++ inference.py
@@ -19,7 +19,9 @@ def inference(conf):
 
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     model_name = conf.model.model_name
-    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fase=False)
+    tokenizer = AutoTokenizer.from_pretrained(model_name, use_fast=True)
+    # use_fast=False로 수정할 경우 -> RuntimeError 발생
+    # RuntimeError: CUDA error: CUBLAS_STATUS_NOT_INITIALIZED when calling `cublasCreate(handle)`
 
     # 이후 토큰을 추가하는 경우 이 부분에 추가해주세요.
     # tokenizer.add_special_tokens()
