13dd62b4dbb6dfab40003d546e9f031348ee2d29
jihoon <jh921204@naver.com>
2022-11-30 07:57:05 +0000

    edit dev path in train_ray.py

--- train_ray.py
+++ train_ray.py
@@ -129,7 +129,7 @@ def train(args, conf):
         model=model,  # the instantiated 🤗 Transformers model to be trained
         args=training_args,  # training arguments, defined above
         train_dataset=RE_train_dataset,  # training dataset
-        eval_dataset=RE_train_dataset,  # evaluation dataset
+        eval_dataset=RE_dev_dataset,  # evaluation dataset
         compute_metrics=utils.compute_metrics,  # define metrics function
         data_collator=data_collator,
         callbacks=[EarlyStoppingCallback(early_stopping_patience=5)],
